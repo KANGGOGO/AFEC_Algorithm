9cdb5f2dfb9636c0ffd8a914689b2b11a2fcbaad
kykim89 <kykim89@coupang.com>
2022-03-26 04:03:34 +0900

    init commit

---
+++ kyuhyoung/scala/src/main/scala/BOJ10172.scala
@@ -0,0 +1,13 @@
+object BOJ10172 {
+  def main(args:Array[String])={
+    println("|\\_/|")
+    println("|q p|   /}")
+    println("( 0 )\"\"\"\\")
+    println("|\"^\"`    |")
+    println("||_/=\\\\__|")
+  }
+}
+
+
+
+
---
+++ kyuhyoung/scala/src/main/scala/BOJ10718.scala
@@ -0,0 +1,6 @@
+object BOJ10718 {
+  def main(args:Array[String])={
+    println("강한친구 대한육군")
+    println("강한친구 대한육군")
+  }
+}
