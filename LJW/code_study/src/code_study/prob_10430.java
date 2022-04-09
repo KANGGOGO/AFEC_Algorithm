3d3544a241111701b2a86135e73497982c072c8c
Jeongwon Lee <85335101+Jarijam@users.noreply.github.com>
2022-04-09 16:30:39 +0900

    labtop  update

---
+++ LJW/code_study/src/code_study/prob_10430.java
@@ -0,0 +1,14 @@
+package code_study;
+
+import java.util.*;
+
+public class prob_10430 {
+	 public static void main(String[] args){
+	        Scanner sc = new Scanner(System.in);
+		 	int A = sc.nextInt();
+		 	int B = sc.nextInt();
+		 	int C = sc.nextInt();
+		 	int ans1 = (A+B)%C는 ((A%C) + (B%C))%C
+        	System.out.println(A-(2541-1998));
+    }
+}    
---
+++ LJW/code_study/src/code_study/prob_109262.java
@@ -0,0 +1,12 @@
+package code_study;
+
+import java.util.*;
+
+public class prob_109262 {
+	 public static void main(String[] args){
+	        Scanner sc = new Scanner(System.in);
+		 	String A;
+	        A = sc.next();
+        	System.out.println(A+"??!");
+    }
+}    
