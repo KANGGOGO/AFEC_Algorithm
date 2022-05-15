de4b2c7ba3bd05fc2123d8749e0cd2df5b73ebd7
KANGGOGO <102231099+KANGGOGO@users.noreply.github.com>
2022-05-15 11:52:42 +0900

    5/15

--- Baekjoon_by_KANG/CHAPTER_4/4-4_3052.py
+++ Baekjoon_by_KANG/CHAPTER_4/4-4_3052.py
@@ -3,3 +3,5 @@ for i in range(10):
     X.append(int(input()) % 42)
 Y = set(X)
 print(len(Y))
+
+# set() = 서로 같은 값을 정리해주는 함수
--- Baekjoon_by_KANG/CHAPTER_5/5-2_4673.py
+++ Baekjoon_by_KANG/CHAPTER_5/5-2_4673.py
@@ -1,17 +1,44 @@
 def d(n):
-    # result = 0
-    # for i in range(n):
-    #     result = result + int(n) % 10**i
-    a = int(n)
-    b = int(n) // 10000
-    c = (int(n) % 10000) // 1000
-    d = ((int(n) % 10000) % 1000) // 100
-    e = (((int(n) % 10000) % 1000) % 100) // 10
-    f = (((int(n) % 10000) % 1000) % 100) % 10
-    result = a + b + c + d + e + f
-    return(result)
+    N = str(n)
+    X = []
+    for i in range(len(N)):
+        X.append(int(N[i]))
+    
+    for j in range(len(X)):
+        n = n + X[j]
+    
+    return n
 
-print(d(358))
+boundary = 10000
+DEL_list = []
+RESULT_LIST = []
 
-n = input()
-print(10**len(n))
+k = 0
+while k < boundary:
+    k += 1
+    DEL_list.append(d(k))
+    DEL_list.sort()
+
+DEL_list_SET = list(set(DEL_list))
+
+DEL_list2 = []
+f = 0
+for f in range(len(DEL_list_SET) + 1):
+    if DEL_list_SET[f] <= boundary:
+        DEL_list2.append(DEL_list_SET[f])
+    else:
+        break
+
+l = 1
+while l <= boundary:
+    RESULT_LIST.append(l)
+    l += 1
+
+m = 0
+for m in range(len(DEL_list2)):
+    RESULT_LIST.remove(DEL_list2[m])
+    m += 1
+
+t = 0
+for t in range(len(RESULT_LIST)):
+    print(RESULT_LIST[t])
